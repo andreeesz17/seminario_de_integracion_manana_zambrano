81e857572146eafac6c1276391f0b5cad8e84051
andreeesz17 <andryzambrano1709@gmail.com>
2026-04-07 11:02:04 -0500

    Clase 07-04-2026

---
+++ modulo1/python_basics/01_hello_world.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+edad = 20
+nombre = "Andresito"
+
+print(f"Hola, mi nombre es {nombre} y tengo {edad} años")
+
+print("Nombre", nombre, "edad", edad)
+
+telefono = 123456789
+print(nombre, edad, telefono, sep=", ")
+
+print(nombre, end = " | ")
+print(edad, end = " | ")
+print(telefono, end = " | ")
+
+print(f"{3.14159:.2f}")
+print(f"{3.14159:,}")
---
+++ modulo1/python_basics/02_input.py
@@ -0,0 +1,6 @@
+nombre=input("¿Cuál es tu nombre?")
+print(f"Hola, {nombre}!")
+
+edad=input("Incluya la edad: ")
+edad_entero=int(edad)
+print(f"Edad: {edad_entero}!")
